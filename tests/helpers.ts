d18a4ba5ddc51ecc106182b07d06e0ae45ffc092
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-02-12 20:24:41 -0300

    initial commit

---
+++ tests/helpers.ts
@@ -0,0 +1,11 @@
+import prisma from "../src/config/database";
+
+export async function cleanDb() {
+  await prisma.movie_genres.deleteMany({});
+  await prisma.streaming_services.deleteMany({});
+  await prisma.movies.deleteMany({});
+}
+
+export async function disconnectDatabase() {
+  await prisma.$disconnect();
+}
